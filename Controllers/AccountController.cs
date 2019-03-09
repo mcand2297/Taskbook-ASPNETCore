ddca32fc9dae2bb961318e01adb5f80a315d13f5
Jorge Castro <mcand2297@gmail.com>
2019-03-09 14:40:21 -0500

    Create AccountController.cs
    Se ha creado el controlador que se encarga de registrar y loegear al usuario de acuerdo con el codigo en el repositorio github: https://github.com/gavilanch/Web-Api-con-Login-dotnet-core-2/blob/master/WebApiPaises/Controllers/AccountController.cs
    
    con el video de apoyo: https://www.youtube.com/watch?v=2OrrKZuqd1k&list=WL&index=19&t=629s

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Taskbook_ASPNETCore.Models;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Taskbook_ASPNETCore.Controllers{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController: ControllerBase{
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            IConfiguration configuration
+        ){
+            _userManager = userManager;
+            _signInManager = signInManager;
+            this._configuration = configuration;
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User model){
+            if(ModelState.IsValid){
+                var user = new User {UserName = model.UserName, Email = model.Email};
+                var result = await _userManager.CreateAsync(user, model.PasswordHash);
+                if(result.Succeeded){
+                    return BuildToken(model);
+                }else{
+                    return BadRequest("UserName or Password invalid");
+                }
+            }else{
+                return BadRequest(ModelState);
+            }
+        }
+        
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> SignIn([FromBody] User  userInfo){
+            if(ModelState.IsValid){
+                var result = await _signInManager.PasswordSignInAsync(userInfo.Email, userInfo.PasswordHash, isPersistent:false, lockoutOnFailure: false);
+                if(result.Succeeded) return BuildToken(userInfo);
+                else{
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return BadRequest(ModelState);
+                }
+            }else{
+                return BadRequest(ModelState);
+            }
+        }
+
+        private IActionResult BuildToken(User userInfo)
+        {
+            var claims = new[]
+            {
+                new Claim( JwtRegisteredClaimNames.UniqueName, userInfo.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["TaskbookLlave_SECRETA"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddHours(48);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+               issuer: "yourdomain.com",
+               audience: "yourdomain.com",
+               claims: claims,
+               expires: expiration,
+               signingCredentials: creds);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = expiration
+            });
+
+        }
+
+    }
+}
